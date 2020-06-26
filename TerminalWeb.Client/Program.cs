7dd338b62f6689f693f97a433022eb8da74655c0
Luiz Fernando <luizfernandoschaves@gmail.com>
2020-06-26 22:09:24 +0000

    client starter

--- TerminalWeb.Client/Program.cs
+++ TerminalWeb.Client/Program.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace TerminalWeb.Client
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        public static void Main() => CreateHostBuilder().Build().Run();
+
+        private static IHostBuilder CreateHostBuilder() =>
+            Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                });
     }
 }
---
+++ TerminalWeb.Client/Worker.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TerminalWeb.Client
+{
+    class Worker : BackgroundService
+    {
+        private HubConnection machineConnection;
+        private HubConnection logConnection;
+
+        public Worker()
+        {
+            machineConnection = CreateHubConnectionStarted("machineHub");
+            logConnection = CreateHubConnectionStarted("logHub");
+        }
+
+        private static HubConnection CreateHubConnectionStarted(string endpoint)
+        {
+            var hubConnectionBuilder = new HubConnectionBuilder().WithUrl($"http://localhost:44395/{endpoint}").Build();
+
+            hubConnectionBuilder.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await hubConnectionBuilder.StartAsync();
+            };
+
+            return hubConnectionBuilder;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Console.WriteLine("Worker running...");
+                await Task.Delay(1000, cancellationToken);
+            }
+        }
+    }
+}
